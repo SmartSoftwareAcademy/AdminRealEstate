544d5bc3f09e680aabc4f82e786b1323abcf6a08
Titus Owuor <54272773+titusowuor30@users.noreply.github.com>
2025-11-09 21:05:42 +0300

    updates

--- AdminRealEstate/settings.py
+++ AdminRealEstate/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',  
-        'PORT': '3307', 
+        'PORT': '3306', 
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
